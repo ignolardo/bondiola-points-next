461cb8ff871dbc8b6776c3bc72690c18863fa6d5
HamerMohamed <cabalazacorfecamalargigante@gmail.com>
2023-03-07 12:43:07 -0300

    first try to google sheets api

--- next.config.js
+++ next.config.js
@@ -8,6 +8,17 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack5: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.child_process = false;
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
--- src/pages/products/index.js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router"
-import { useState } from "react";
-
-module.exports = function Products()
-{
-    return (<div>Productos</div>)
-}
